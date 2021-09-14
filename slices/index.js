1303bc03fbadadaa10621debd5e462dce2a295ee
dayhaysoos <ndejesus1227@gmail.com>
2021-09-14 11:25:12 -0400

    remove product slice

--- slices/index.js
+++ slices/index.js
@@ -1,4 +1,3 @@
-export { default as ProductSlice } from './ProductSlice';
 export { default as Title } from './Title';
 export { default as OffsetHero } from './OffsetHero';
 export { default as MarketingCopy } from './MarketingCopy';
